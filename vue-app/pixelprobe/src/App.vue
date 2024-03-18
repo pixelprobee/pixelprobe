7d4f344e2ff7a16edafea436b8ae7610e3309f57
pixelprobee <pixelprobe@zohomail.in>
2024-03-18 17:46:42 +0530

    Added vue project

---
+++ vue-app/pixelprobe/src/App.vue
@@ -0,0 +1,9 @@
+<script setup>
+import SubmitButton from './components/SubmitButton.vue'
+import Images from './components/Images.vue'
+</script>
+<template>
+    <h1>Pixel Probe</h1>
+    <Images/>
+    <SubmitButton/>
+</template>
---
+++ vue-app/pixelprobe/src/components/Images.vue
@@ -0,0 +1,4 @@
+<template>
+    <img src="https://www.simplilearn.com/ice9/free_resources_article_thumb/Types_of_Artificial_Intelligence.jpg" class="rounded float-start" style="width:50%">
+    <img src="https://static.vecteezy.com/system/resources/previews/023/826/514/large_2x/ai-artificial-intelligence-humanoid-side-portrait-view-with-blue-and-orange-vibrant-neon-and-copy-space-artificial-intelligence-technology-concept-ai-generated-illustration-free-photo.jpg" class="rounded float-end" style="width:50%">
+</template>
